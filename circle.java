aebac16401f6e4397af63958363a088d4d747d8d
leminhthuhannah-netizen <leminhthuhannah@gmail.com>
2026-05-11 09:32:26 +0700

    Code day du cua hinh tron

--- circle.java
+++ circle.java
@@ -2,6 +2,7 @@ package kethua;
 
 public class circle extends shape {
     private double radius = 1.0;
+    //Contructor
     public circle(){}
     public circle(double radius){
         this.radius = radius;
@@ -9,6 +10,18 @@ public class circle extends shape {
     public circle(double radius,String color,boolean filled){
         super(color,filled); // gọi shape đầu tiên nếu ko sẽ lỗi 
         this.radius = radius;
+    }
+    // getters setters
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+    //Để biến toString thành riêng của circle ta cần thay đổi trong nó  -- ghi đè
+    @Override
+    public String toString() {
+        return "Circle["+ super.toString()+"radius=" + radius +"]";
     }
     
 }
